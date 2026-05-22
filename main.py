1b2ae90582923c11da45a76ae25c4b95bd95084c
henrique <hcavoliveira@proton.me>
2026-05-21 22:01:53 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+
+result_01 = pd.read_csv('project_sql_result_01.csv')
+result_04 = pd.read_csv('project_sql_result_04.csv')
+result_07 = pd.read_csv('project_sql_result_07.csv')
